f1af09de7a7dd94b168da966eb21392d751aef51
maanviGupta04 <maanvi.gupta04@gmail.com>
2026-01-02 20:32:44 +0530

    Add object-based dictionary example

---
+++ basics/q3_variables.js
@@ -0,0 +1,18 @@
+const dict={
+   patience: "The capacity to accept or tolerate delay without frustration",
+   kindness: "The quality of being friendly, generous, and considerate",
+   courage: "The ability to face fear, pain, or difficulty with strength",
+   peace: "A state of harmony and freedom from disturbance",
+   respect: "A feeling of deep admiration for someone or something",
+   knowledge: "Information and understanding gained through learning or experience",
+   
+
+}
+//Prints the dict
+console.log(dict);
+
+//Prints the key values
+console.log("The keys are",Object.keys(dict));
+
+//Prints the values corresponding to keys
+console.log("The values are",ObjectObject.values(dict));
