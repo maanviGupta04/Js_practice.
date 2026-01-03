3c14efc969e584fea7de3a942242f8c27e4fa2d6
maanviGupta04 <maanvi.gupta04@gmail.com>
2026-01-03 23:59:59 +0530

    Add switch case example for card luck

---
+++ basics/02_expressions_conditions./q2_switch-case.js
@@ -0,0 +1,31 @@
+let cardno=4;
+switch(cardno){
+    case 1:
+        console.log("HARDLUCK");
+        break;
+        
+    case 2:
+        console.log("BADLUCK");
+        break;
+
+    case 3:
+        console.log("BAD-DAY");
+        break;
+
+    case 4:
+        console.log("MOST LUCKY");
+        break;
+
+    case 5:
+        console.log("LUCKY");
+        break;
+
+    case 6:
+        console.log("LUCKIER");
+        break;
+
+    default:
+        console.log("GOD'S PLAN")
+
+
+}
