4bda7675d4303865f78ed5a978eaa05784fa1112
maanviGupta04 <maanvi.gupta04@gmail.com>
2026-01-10 14:37:09 +0530

    Iterate over object keys to print student marks

---
+++ basics/03_loops&functions/q1_object_iteration.js
@@ -0,0 +1,14 @@
+ let obj = {
+    "Maanvi": 98,
+    "Riya": 70,
+    "Bhoomi": 73
+}
+
+let keys =Object.keys(obj)
+// console.log(keys)
+
+for (let i = 0; i < keys.length; i++) {
+  
+    console.log(student +":"+obj[student])
+}
+
