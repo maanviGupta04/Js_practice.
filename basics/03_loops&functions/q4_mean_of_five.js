26dd1844ecf61f057af59d07ee0ca0d5d0178803
maanviGupta04 <maanvi.gupta04@gmail.com>
2026-01-11 21:23:18 +0530

    Add function to calculate mean of five numbers

---
+++ basics/03_loops&functions/q4_mean_of_five.js
@@ -0,0 +1,11 @@
+
+function meanOfFive(a, b, c, d, e) {
+    let sum = a + b + c + d + e; 
+    let mean = sum / 5;          
+    return mean;
+}
+
+let result = meanOfFive(10, 20, 30, 40, 50);
+console.log("Mean is:", result);
+
+
