fbd5a08802c6c5e90cfd53aaefd7c04878f2f09f
maanviGupta04 <maanvi.gupta04@gmail.com>
2026-01-12 21:57:54 +0530

    Practice string transformation methods in JavaScript

---
+++ basics/04_strings/q3_string-transform-methods.js
@@ -0,0 +1,10 @@
+let is_name = "Maanvi";
+
+console.log(is_name.toLowerCase());
+console.log(is_name.toUpperCase());
+console.log(is_name.length);
+console.log(is_name.charAt(0));
+console.log(is_name.slice(0, 3));
+console.log(is_name.replace("aa", "a"));
+console.log(is_name.repeat(2));
+console.log(is_name.trim());
