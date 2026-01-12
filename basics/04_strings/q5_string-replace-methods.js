5224e5ab8bee3134908c4cc914615f7755acbeaf
maanviGupta04 <maanvi.gupta04@gmail.com>
2026-01-12 22:11:50 +0530

    Practice string replace method with regex

--- basics/04_strings/q5_string-replace-methods.js
+++ basics/04_strings/q5_string-replace-methods.js
@@ -1,1 +1,8 @@
+let string = "Maanvi";
 
+console.log(string.replace("n", "4"));
+console.log(string.replace("a", "@"));
+console.log(string.replace(/a/g, "@"));
+console.log(string.replace("Maanvi", "Hello"));
+console.log(string.replace(/i/g, "!"));
+console.log(string.replace(/[aeiou]/g, "*"));
