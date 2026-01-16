776c7239325844683c570dacdbf4fda2db53f068
maanviGupta04 <maanvi.gupta04@gmail.com>
2026-01-16 21:40:16 +0530

    Collect user inputs in array using loop

---
+++ basics/arrays/q1_array-loop.js
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <script>
+    
+         let numbers=[];
+
+         for (let i = 0; i < 10; i++) {
+            let user=Number(prompt("Enter the number"));
+            numbers.push(user);
+         }
+
+         console.log(numbers);
+
+    </script>
+</body>
+</html>
+
+
