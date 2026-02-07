c30fe94cad7a969a204cb5f02d074479424c3992
maanviGupta04 <maanvi.gupta04@gmail.com>
2026-02-07 22:43:56 +0530

    Add Complex number class using ES6

---
+++ basics/10_class/q1_complex-number.js
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+  </head>
+  <body>
+  
+    <script>
+      class Complex {
+        constructor(real, imaginary) {
+          this.real = real;
+          this.imaginary = imaginary;
+        }
+      }
+      let newnum = new Complex(32, 87);
+      console.log(newnum);
+    </script>
+  </body>
+</html>
